edb9925a73c8d64fd70f759e92cf0301bbeb3098
Josh Birch <2312292@chester.ac.uk>
2025-04-02 11:41:55 +0100

    Done with errors

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,133 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        PokeballGame game = new PokeballGame();
+        Scanner inputScanner = new Scanner(System.in);
+
+        while (true)
+        {
+            //
+            System.out.println("What do you want to do? [L]ook for a Pokemon, " +
+                    "view your [F]ound Pokemon, [I]tems, " +
+                    "[V]isit Pokestop, [E]xit?");
+            String input = inputScanner.nextLine();
+
+            //
+            switch (input.toUpperCase())
+            {
+                case "L":
+                    findPokemon(game);
+                    break;
+                case "F":
+                    displayInventory(game);
+                    break;
+                case "V":
+                    System.out.println(game.visitPokestop());
+                    break;
+                case "I":
+                    displayBallCounts(game);
+                    break;
+                case "E":
+                    return;    // exits programme
+                default:
+                    break;    // invalid input does nothing
+            }
+        }
+    }
+
+    private static void findPokemon(PokeballGame game)
+    {
+        if(game.lookForPokemon())
+        {
+            Pokemon pokemon = game.getFoundPokemon();
+            System.out.println("Pokemon found: " + pokemon.getName() +
+                    ", Combat Points: " + pokemon.getCombatPoints());
+
+            while(true)
+            {
+                //
+                displayBallCounts(game);
+                System.out.println("Choose: [B]all, [G]reat ball or [U]ltra ball ([E]xit):");
+                Scanner inputScanner = new Scanner(System.in);
+                String ballType = inputScanner.nextLine();
+
+                if(ballType.equalsIgnoreCase("E"))
+                {
+                    break;
+                }
+
+                //
+                PokeBall ball = PokeBall.STANDARD;
+
+                switch(ballType.toUpperCase())
+                {
+                    case "U":
+                        ball = PokeBall.ULTRA;
+                        break;
+                    case "G":
+                        ball = PokeBall.GREAT;
+                        break;
+                }
+
+                Pokemon.CaptureResult result = game.attemptToCatchPokemon(ball);
+                if(result == null)
+                {
+                    System.out.println("Attempt failed. No Pokeballs!");
+                    return;
+                }
+
+                switch (result)
+                {
+                    case CAUGHT:
+                        System.out.println("You caught " + pokemon.getName() +
+                                "! Combat Points: " + pokemon.getCombatPoints());
+                        return;    // catch loop exited - pokemon caught
+                    case FAIL:
+                        System.out.println("Failed to catch Pokemon!");
+                        break;    // catch loop restarts
+                    case FLEE:
+                        System.out.println("The Pokemon has fled!");
+                        return;    // exited
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Did not find Pokemon.");
+        }
+    }
+
+    private static void displayBallCounts(PokeballGame game)
+    {
+        int ballCount = game.getBallCount(PokeBall.STANDARD);
+        int greatBallCount = game.getBallCount(PokeBall.GREAT);
+        int ultraBallCount = game.getBallCount(PokeBall.ULTRA);
+
+        System.out.println("You have " + ballCount + "balls, " +
+                greatBallCount + " great balls, and " + ultraBallCount +  " ultra balls.");
+    }
+
+    private static void displayInventory(PokeballGame game)
+    {
+        ArrayList<Pokemon> pokemons = game.getCaughtPokemon();
+
+        System.out.println("Pokemon you've caught:");
+        if(!pokemons.isEmpty())
+        {
+            for (Pokemon pokemon : pokemons)
+            {
+                System.out.println(pokemon.getName() + ": " + pokemon.getCombatPoints() + " combat points");
+            }
+            System.out.println();
+        }
+        else
+        {
+            System.out.println("You have not caught any Pokemon.\n");
+        }
+
     }
 }
